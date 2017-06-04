66937069720b3f2db72e9b5300537353623eaa90
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2017-06-04 00:52:34 -0400

    disagreement checking.

--- admin.py
+++ admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Import models
+from sourcenet_analysis.models import Reliability_Names
+from sourcenet_analysis.models import Reliability_Ties
+
+#admin.site.register( Reliability_Names )
+#admin.site.register( Reliability_Ties )
--- views.py
+++ views.py
@@ -143,7 +143,10 @@ def build_reliability_name_detail_string( reliability_names_id_IN,
                                           delimiter_IN = "|",
                                           prefix_IN = "| ",
                                           suffix_IN = " |",
-                                          default_status_IN = "CORRECT" ):
+                                          default_status_IN = "CORRECT",
+                                          protocol_IN = "http",
+                                          host_IN = "research.local",
+                                          app_path_IN = "sourcenet/" ):
     
     '''
     Accepts Reliability_Names instance, and optional delimiter, prefix, and
@@ -250,11 +253,11 @@ def build_reliability_name_detail_string( reliability_names_id_IN,
                 detail_string += str( reliability_names_id )
                 detail_string += " " + delimiter_IN + " Article ["
                 detail_string += str( article_id )
-                detail_string += "](http://research.local/sourcenet/sourcenet/article/article_data/view_with_text/?article_id="
+                detail_string += "](" + str( protocol_IN ) + "://" + str( host_IN ) + "/" + str( app_path_IN ) + "sourcenet/article/article_data/view_with_text/?article_id="
                 detail_string += str( article_id )
                 detail_string += ") " + delimiter_IN + " Article_Data ["
                 detail_string += str( article_data_id )
-                detail_string += "](http://research.local/sourcenet/sourcenet/article/article_data/view/?article_id="
+                detail_string += "](" + str( protocol_IN ) + "://" + str( host_IN ) + "/" + str( app_path_IN ) + "sourcenet/article/article_data/view/?article_id="
                 detail_string += str( article_id )
                 detail_string += "&article_data_id_select="
                 detail_string += str( article_data_id )
@@ -312,6 +315,193 @@ def build_reliability_name_detail_string( reliability_names_id_IN,
     return detail_string_OUT
 
 #-- END method build_reliability_name_detail_string() --#
+
+
+def build_reliability_name_summary_string( reliability_names_id_IN,
+                                           delimiter_IN = "|",
+                                           prefix_IN = "| ",
+                                           suffix_IN = " |",
+                                           default_status_IN = "CORRECT",
+                                           protocol_IN = "http",
+                                           host_IN = "research.local",
+                                           app_path_IN = "sourcenet/",
+                                           default_error_IN = "MISSED" ):
+    
+    '''
+    Accepts Reliability_Names instance, and optional delimiter, prefix, and
+        suffix.  Retrieves the Article_Data, and Article_Subject(s) that the
+        Reliability_Name refers to.  Uses information from all to build a detail
+        string that contains:
+        - Reliability_Names ID.
+        - Article ID.
+        Article_Data that 
+    '''
+    
+    # return reference
+    detail_string_OUT = None
+    
+    # declare variables
+    detail_string_list = []
+    detail_string = ""
+    reliability_names_id = -1
+    reliability_names_qs = None
+    reliability_names_instance = None
+    person_name = ""
+    related_article = None
+    article_id = -1
+    index_list = []
+    current_index = -1
+    
+    # declare variables - retrieve information from Reliability_Names row.
+    current_suffix = ""
+    article_data_id = -1
+    article_data_qs = None
+    article_data_instance = None
+    article_data_coder_id = -1
+    article_data_id_list = []
+    person_type_column_name = ""
+    person_type = ""
+    article_person_id_column_name = ""
+    article_person_id = -1
+    article_person_qs = None
+    article_person_instance = None
+    
+    # declare variables - output rendering.
+    article_data_link_list = []
+    article_data_link = ""
+    
+    # get information for output
+    reliability_names_id = reliability_names_id_IN
+    if ( ( reliability_names_id is not None ) and ( reliability_names_id > 0 ) ):
+    
+        # get Reliability_Names instane.
+        reliability_names_qs = Reliability_Names.objects.all()
+        reliability_names_instance = reliability_names_qs.get( pk = reliability_names_id )
+        
+        # get person name
+        person_name = reliability_names_instance.person_name
+        
+        # get related article.
+        related_article = reliability_names_instance.article
+        article_id = related_article.id
+        
+        # Get list of related Article_Data ids.
+        article_data_id_list = []
+        article_data_link_list = []
+        
+        # initialize
+        index_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+        for current_index in index_list:
+        
+            # see if there is an Article_Data ID.
+            current_suffix = Reliability_Names.FIELD_NAME_SUFFIX_ARTICLE_DATA_ID
+            article_data_id = reliability_names_instance.get_field_value( current_index, current_suffix )
+            if ( article_data_id is not None ):
+    
+                # add to list
+                article_data_id_list.append( article_data_id )
+
+                # we have an ID value, try to get Article_Data...
+                article_data_qs = Article_Data.objects.all()
+                article_data_instance = article_data_qs.get( pk = article_data_id )
+                article_data_coder_id = article_data_instance.coder_id
+                
+                # ...person_type...
+                current_suffix = Reliability_Names.FIELD_NAME_SUFFIX_PERSON_TYPE
+                person_type = reliability_names_instance.get_field_value( current_index, current_suffix )
+
+                # ...and based on Type, Article_Subject or Article_Author.
+                # is there also a person ID?
+                current_suffix = Reliability_Names.FIELD_NAME_SUFFIX_ARTICLE_PERSON_ID
+                article_person_id = reliability_names_instance.get_field_value( current_index, current_suffix )
+                if ( ( article_person_id is not None ) and ( article_person_id > 0 ) ):
+                
+                    # get Article_Subject or Article_Author
+                    if ( person_type == Reliability_Names.PERSON_TYPE_AUTHOR ):
+                    
+                        # author.
+                        article_person_qs = Article_Author.objects.all()
+                        article_person_instance = article_person_qs.get( pk = article_person_id )
+                        
+                    elif ( ( person_type == Reliability_Names.SUBJECT_TYPE_MENTIONED )
+                        or ( person_type == Reliability_Names.SUBJECT_TYPE_QUOTED ) ):
+                        
+                        # subject.
+                        article_person_qs = Article_Subject.objects.all()
+                        article_person_instance = article_person_qs.get( pk = article_person_id )
+                    
+                    else:
+                    
+                        article_person_instance = None
+
+                    #-- END check of person type. --#
+                
+                #-- END check to see if article_person_id --#
+                
+                # create link (very basic for now):
+                article_data_link = "[" + str( article_data_id ) + " (coder=" + str( article_data_coder_id ) + ")]"
+                article_data_link += "(" + str( protocol_IN ) + "://" + str( host_IN ) + "/" + str( app_path_IN ) + "sourcenet/article/article_data/view/?article_id="
+                article_data_link += str( article_id )
+                article_data_link += "&article_data_id_select="
+                article_data_link += str( article_data_id )
+                article_data_link += ")"
+                
+                # add to list.
+                article_data_link_list.append( article_data_link )
+
+            #-- END check to see if Article_Data ID. --#
+                        
+        #-- END loop over indexes. --#
+        
+        # build detail string.
+        detail_string = prefix_IN
+        
+        # ==> Reliability_Names ID
+        detail_string += str( reliability_names_id )
+
+        detail_string += " " + delimiter_IN + " "
+        
+        # ==> person name
+        detail_string += person_name
+        
+        detail_string += " " + delimiter_IN + " "
+
+        # ==> Article ID and link
+        detail_string += "Article "
+        detail_string += "[" + str( article_id ) + "]"
+        detail_string += "(" + str( protocol_IN ) + "://" + str( host_IN ) + "/" + str( app_path_IN ) + "sourcenet/article/article_data/view_with_text/?article_id="
+        detail_string += str( article_id )
+        detail_string += ")"
+        
+        detail_string += " " + delimiter_IN + " "
+
+        # ==> Article_Data IDs and links
+        detail_string += "Article_Data: "
+        detail_string += "; ".join( article_data_link_list )
+        
+        # ==> status
+        detail_string += " " + delimiter_IN + " " + default_status_IN
+        
+        # ==> error
+        detail_string += " " + delimiter_IN + " " + default_error_IN
+        
+        # ==> notes
+        detail_string += " " + delimiter_IN + " None"
+        
+        detail_string += suffix_IN
+
+        detail_string_OUT = detail_string
+
+    else:
+    
+        # no ID passed in.  Return None.
+        detail_string_OUT = None
+    
+    #-- END check to see if Reliabilty_Names ID passed in. --#
+    
+    return detail_string_OUT
+
+#-- END method build_reliability_name_summary_string() --#
 
 
 #===============================================================================
@@ -960,12 +1150,17 @@ def reliability_names_disagreement_view( request_IN ):
                                 # add all tags in list
                                 reliability_names_instance.tags.remove( *reliability_names_action_tag_list )
                                                                 
+                                # build a detail string:
+                                detail_string = build_reliability_name_summary_string( reliability_names_id )
+                                detail_string_list.append( detail_string )
+    
                             #-- END loop over selected IDs. --#
                             
                             # update the action details list.
                             action_summary = "Removed tag(s): " + str( reliability_names_action_tag_list ) + " from " + str( remove_tag_counter ) + " Reliability_Names records."
                             action_detail_list.append( action_summary + " ==> IDs: " + str( select_id_list ) )
-                            
+                            action_detail_list.extend( detail_string_list )
+                           
                         else:
                         
                             # Nothing selected.
